7d20bc8d21724f88c11df1308272ffe958ced621
JacobHaake <jhaake18@gmail.com>
2023-11-26 00:30:53 -0500

    Removed serial implementation

--- src/samplesort.cpp
+++ src/samplesort.cpp
@@ -48,131 +48,7 @@ void generateRandNums(int inputNums[], int numNums, int randomNumBound) {
         inputNums[i] = (rand() % randomNumBound);
 }
 
-
-void bubblesort(int inputNums[], int start, int end) {
-    //std::cout << "Bubblesorting" << std::endl;
-    int i, j, temp;
-    for (i = start; i < end - 1; ++i) {
-        for (j = start; j < start + end - i - 1; ++j) {
-            if (inputNums[j] > inputNums[j + 1]) {
-                temp = inputNums[j + 1];
-                inputNums[j + 1] = inputNums[j];
-                inputNums[j] = temp;
-            }
-        }
-    }
-}
-
-// Baseline Samplesort
-//
-// STEP 1: Since this is recursive, bubblesort does the sorting at the innermost call when numNums is less than numOfBuckets
-//         Drop the number of samplesPerBucket to 1 if that allows the samplesort to continue. Otherwise, just call bubblesort on the rest
-//
-// STEP 2: Take numOfBuckets * samplesPerBucket samples from inputNums (since inputNums is assumed to be random, I just pull values from the front)
-//         Sort these samples using bubblesort (could use samplesort here but it is likely a small array anyway so just bubblesort it)
-//         Take numOfBuckets - 1 values evenly from the samples. These are your splitters and should vaguely divide the 
-//         entire inputNums into numOfBuckets buckets
-//
-// STEP 3: Sort all values in inputNums into a bucket (a bucket is defined by two adjacent splitters)
-//         For parallel implementation, it can be beneficial to segment inputNums evenly up and stick a processor on each one
-//         This is why the implementation segments the values numOfBuckets ways (bc numOfProcessors is what I pass into the numOfBuckets argument)
-//
-// STEP 4: Copy all buckets into inputNums in order. Sort each bucket in inputNums
-//         Once each bucket is sorted in place, there is no work to do to combine the buckets because they are already in order
-//         Samplesort is recursively called to sort each bucket in place
-void samplesort(int inputNums[], int start, int end, int numOfBuckets, int samplesPerBucket) {
-    std::cout << "start: " << std::to_string(start)
-        << ", end: " << std::to_string(end)
-        << ", numOfBuckets: " << std::to_string(numOfBuckets)
-        << ", samplesPerBucket: " << std::to_string(samplesPerBucket) << std::endl;
-    int i, j, k;
-    // numNums is the number of numbers that we are sorting
-    int numNums = end - start;
-
-    // STEP 1
-    // If there are not enough inputNums to take the quantity of samples per bucket, then just set samplesPerBucket to 1
-    if (numOfBuckets * samplesPerBucket > numNums) {
-        // Unsure if this is supposed to have equality flipped
-        if (numNums / numOfBuckets >= 1) {
-            samplesPerBucket = 1;
-        }
-        // If there are fewer inputNums than specified numOfBuckets, then just bubblesort the rest bc it is likely a small set left
-        else {
-            bubblesort(inputNums, start, end);
-            std::cout << "bubblesort function end" << std::endl;
-            return;
-        }
-    }
-
-    // STEP 2
-    // Creating an array of samples so that we can find pivot points for the algorithm
-    int sortedSamples[(numOfBuckets - 1) * samplesPerBucket];
-    for (i = start; i < start + (numOfBuckets - 1) * samplesPerBucket; ++i)
-        sortedSamples[i - start] = inputNums[i];
-
-    // Sorting the collection of samples with bubble sort
-    bubblesort(sortedSamples, 0, (numOfBuckets - 1) * samplesPerBucket);
-
-    // Condensing the sortedSamples into just splitters between the buckets
-    int splitters[numOfBuckets - 1];
-    for (i = 0; i < numOfBuckets - 1; ++i)
-        splitters[i] = sortedSamples[i * samplesPerBucket];
-
-    // STEP 3
-    // Going through inputNums and sorting each value into a bucket (each bucket is stored as a vector)
-    // NOTE: The initial double nesting of for loops is not necessary for serial purposes. I could have made the first
-    //        two for loops into a single for loop, but I think it will be easier to parallelize it when set up this way
-    std::vector<int> buckets[numOfBuckets];
-    // This breaks the initial inputNums into equal segments, then loops through each value and puts it into the bucket it belongs to
-    for (i = 0; i < numOfBuckets; ++i) {
-        for (j = start + (i * (numNums / numOfBuckets)); j < start + ((i + 1) * (numNums / numOfBuckets)); ++j) {
-            // Looping through to check which bucket the current value belongs to, 
-            // then pushing it into the bucket if it falls in that bound
-            for (k = 0; k < numOfBuckets - 1; ++k) {
-                if (inputNums[j] <= splitters[k]) {
-                    buckets[k].push_back(inputNums[j]);
-                    break;
-                }
-                if (k == numOfBuckets - 2)
-                    buckets[numOfBuckets - 1].push_back(inputNums[j]);
-            }
-        }
-    }
-
-    // Handling the remainder values at the end of the list which were not accounted for above
-    // The inner for loop is the same as above bc it just sorts the value in question into the right bucket
-    // If you have 20 buckets, but 23 values in the array, the above loop will handle the first 20 values
-    // and this loop will handle the remaining three bc they are kinda outliers
-    for (i = start + numNums - (numNums % numOfBuckets); i < end; ++i) {
-        for (k = 0; k < numOfBuckets - 1; ++k) {
-            if (inputNums[i] <= splitters[k]) {
-                buckets[k].push_back(inputNums[i]);
-                break;
-            }
-            if (k == numOfBuckets - 2)
-                buckets[numOfBuckets - 1].push_back(inputNums[i]);
-        }
-    }
-
-    // STEP 4
-    // Goes one-by-one through each bucket and copies their values into inputNums
-    int runningIndex = start;
-    for (i = 0; i < numOfBuckets; ++i) {
-        int bucketSize = buckets[i].size();
-        for (j = 0; j < bucketSize; ++j)
-            inputNums[runningIndex + j] = buckets[i].at(j);
-        // Once the values of a bucket are copied into inputNums, we
-        // recursively call samplesort to sort those values in place.
-        std::cout << "Calling new samplesort" << std::endl;
-        samplesort(inputNums, runningIndex, runningIndex + bucketSize, numOfBuckets, samplesPerBucket);
-        runningIndex += bucketSize;
-    }
-    std::cout << "function ending" << std::endl;
-}
-
-
 void bubblesort(std::vector<int> &inputNums, int start, int end) {
-    //std::cout << "Bubblesorting" << std::endl;
     int i, j, temp;
     for (i = start; i < end - 1; ++i) {
         for (j = start; j < start + end - i - 1; ++j) {
@@ -185,7 +61,7 @@ void bubblesort(std::vector<int> &inputNums, int start, int end) {
     }
 }
 
-void* testFunc2(void* vArgs)
+void* testFunc(void* vArgs)
 {
     // Convert void* back to struct*
     testArgs* tArgs = static_cast<testArgs*>(vArgs);
@@ -274,7 +150,7 @@ void* samplesort(void* vArgs) {
             args->threads->push_back(newThread);
 
             // Start up thread
-            pthread_create(newThread, NULL, testFunc2, static_cast<void*>(& tArgs));
+            pthread_create(newThread, NULL, testFunc, static_cast<void*>(& tArgs));
             pthread_mutex_unlock(&(args->mtx));
         }
     }
